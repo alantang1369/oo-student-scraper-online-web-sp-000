847cfd9bbf99066e3f5395f4c49a1fc597a4f5aa
Alan Tang <giabao_tang@yahoo.com>
2020-10-27 23:29:06 +0000

    Automatically backed up by Learn

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -10,16 +10,16 @@ describe "Scraper" do
                             :linkedin=>"https://www.linkedin.com/in/jmburges",
                             :github=>"https://github.com/jmburges",
                             :blog=>"http://joemburgess.com/",
-                           # :profile_quote=>"\"Reduce to a previously solved problem\"",
-                            #:bio=>
-  #"I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}}
+                            :profile_quote=>"\"Reduce to a previously solved problem\"",
+                            :bio=>
+  "I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}}
 
   let!(:student_david_hash) {{:linkedin=>"https://www.linkedin.com/in/david-kim-38221690",
  :github=>"https://github.com/davdkm",
- #:profile_quote=>
- # "\"Yeah, well, you know, that's just, like, your opinion, man.\" - The Dude",
- #:bio=>
- # "I'm a southern California native seeking to find work as a full stack web #developer. I enjoying tinkering with computers and learning new things!"}}
+ :profile_quote=>
+  "\"Yeah, well, you know, that's just, like, your opinion, man.\" - The Dude",
+ :bio=>
+  "I'm a southern California native seeking to find work as a full stack web #developer. I enjoying tinkering with computers and learning new things!"}}
 
   describe "#scrape_index_page" do
     it "is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student" do
